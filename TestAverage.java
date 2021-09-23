9ea4ca9a848cdcd2c010d9e8e07ce4f0314e21b0
oliviamurawski <91077851+oliviamurawski@users.noreply.github.com>
2021-09-23 15:24:07 -0400

    Add files via upload

---
+++ TestAverage.java
@@ -0,0 +1,24 @@
+public class TestAverage{
+   public static void main(String[] args){
+   
+    // take two numbers
+    double num1 = 96;
+    double num2 = 96;
+    double num3 = 94;
+
+    // declare sum variable
+    // and initialize with 0
+    double sum = 0.0;
+    // declare average variable
+    double avg = 0.0;
+
+    // calculate the sum value
+    sum = num1 + num2 + num3;
+    // calculate the average value
+    avg = sum/3;
+
+    // display result
+    System.out.println("Average: " + avg );
+  }
+}   
+   
